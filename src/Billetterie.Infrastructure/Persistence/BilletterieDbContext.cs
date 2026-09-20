7b927a802c58ebff3f5926d8fb96328493e8a37b
soulaymanebenaddi <soulaymanebenaddi@gmail.com>
2026-09-20 12:05:15 -0400

    feature: DbSet added for Domain entities

--- src/Billetterie.Infrastructure/Persistence/BilletterieDbContext.cs
+++ src/Billetterie.Infrastructure/Persistence/BilletterieDbContext.cs
@@ -1,9 +1,23 @@
+using Billetterie.Domain.Events;
+using Billetterie.Domain.Venues;
 using Microsoft.EntityFrameworkCore;
 
 namespace Billetterie.Infrastructure.Persistence;
 
 public class BilletterieDbContext : DbContext
 {
+    public DbSet<Event> Events => Set<Event>();
+
+    public DbSet<Venue> Venues => Set<Venue>();
+
+    public DbSet<VenueSpace> VenueSpaces => Set<VenueSpace>();
+
+    public DbSet<Section> Sections => Set<Section>();
+
+    public DbSet<Row> Rows => Set<Row>();
+
+    public DbSet<Seat> Seats => Set<Seat>();
+
     public BilletterieDbContext(
         DbContextOptions<BilletterieDbContext> options)
         : base(options)
